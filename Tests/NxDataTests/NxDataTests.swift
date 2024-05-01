bf1206138221e8549e4f0d4eee8d01431146cc06
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-05-01 12:34:44 -0400

    Updated unit tests

--- Tests/NxDataTests/NxDataTests.swift
+++ Tests/NxDataTests/NxDataTests.swift
@@ -1,11 +1,15 @@
+import Foundation
 import XCTest
+import System
 @testable import NxData
 
 final class NxDataTests: XCTestCase {
     
+    let path = Bundle.module.path(forResource: "Data", ofType: NxFile.fileExtension)
+    
     func testNxDataHeader() throws {
         
-        guard let path = Bundle.module.path(forResource: "Data", ofType: NxFile.fileExtension) else {
+        guard let path else {
             return
         }
         
@@ -15,5 +19,22 @@ final class NxDataTests: XCTestCase {
         XCTAssertEqual(header.nodeBlockOffset, 52)
         XCTAssertEqual(header.stringCount, 43081)
         XCTAssertEqual(header.stringOffsetTableOffset, 115612536)
+    }
+    
+    func testStrings() async throws {
+        
+        guard let path else {
+            return
+        }
+        
+        let file = try NxFile(path: FilePath(path))
+        
+        let stringCount = try await file.string.count
+        XCTAssertEqual(stringCount, 43081)
+        
+        let img0001 = try await file.string[1]
+        XCTAssertEqual(img0001, "0001.img")
+        
+        
     }
 }

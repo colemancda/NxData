5570c9bfc3787acfa6e3a4924ffbedf06242ada9
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-05-01 14:09:01 -0400

    Add `Audio`

---
+++ Sources/NxData/Audio.swift
@@ -0,0 +1,51 @@
+//
+//  Audio.swift
+//
+//
+//  Created by Alsey Coleman Miller on 5/1/24.
+//
+
+import Foundation
+
+/// Nx Audio Data
+public struct Audio: Equatable, Hashable, Codable, Sendable {
+    
+    /// Audio Identifier
+    public let id: ID
+    
+    /// Audio data
+    public let data: Data
+}
+
+// MARK: - Supporting Types
+
+public extension Audio {
+    
+    /// Nx Audio ID
+    struct ID: RawRepresentable, Equatable, Hashable, Codable, Sendable {
+        
+        public let rawValue: UInt32
+        
+        public init(rawValue: UInt32) {
+            self.rawValue = rawValue
+        }
+    }
+}
+
+// MARK: - ExpressibleByIntegerLiteral
+
+extension Audio.ID: ExpressibleByIntegerLiteral {
+    
+    public init(integerLiteral value: UInt32) {
+        self.init(rawValue: value)
+    }
+}
+
+// MARK: - CustomStringConvertible
+
+extension Audio.ID: CustomStringConvertible {
+    
+    public var description: String {
+        rawValue.description
+    }
+}
